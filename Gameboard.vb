2a49b850227658867fc7efc32e355ee0da181696
Pearlandinjun <sean.m.kruse@gmail.com>
2016-04-07 00:38:10 -0500

    test

--- Gameboard.vb
+++ Gameboard.vb
@@ -1,7 +1,7 @@
 ﻿Public Class Gameboard
     Private vaderFlag As Integer
     Public stone(2) As Drawing.Color
-    Private Dim board(6, 6) As Integer
+    Private board(6, 6) As Integer
     Public player1 As player = New player()
     Public player2 As player = New player()
     Private score1 As Integer
@@ -20,22 +20,22 @@
     Public Sub setDiff(ByVal Vader As Integer)
         vaderFlag = Vader
     End Sub
-    
+
     Public Sub setPlayer(ByVal Nm1 As String, ByVal Nm2 As String)
         player1.name = Nm1
         player1.turnNum = 1
         player2.name = Nm2
         player2.turnNum = 2
     End Sub
-    
+
     Public Sub easyVader()
-    
+
         Dim vaderTurn As Integer                        'variable to hold the numerical value for the AI's turn
         Dim playerTurn As Integer                       'variable to hold the numerical value for the player's turn
         Dim flagFound As Boolean = False                'variable used to control the while loop
         Dim vaderMove1 As Integer                       'variable to hold AI selection of the first dimension of the array
         Dim vaderMove2 As Integer                       'variable to hold AI selection of the second dimension of the array
-        
+
         If player1.name = "Vader" Then                  'if statement to set the vaderTurn and playerTurn variables
             vaderTurn = 1                               'to their corresponding values to use in Vader's move selection
             playerTurn = 2
@@ -43,22 +43,21 @@
             vaderTurn = 2
             playerTurn = 1
         End If
-        
-        Do                                                           'Do While loop to select a random position and check it 
-                                                                     'for vader's selection. If position is taken the loop iterates again
-                                                                     'and checks a new position
+
+        Do                                                           'Do While loop to select a random position and check it for vader's selection. If position is taken the loop iterates again and checks a new position
+
             vaderMove1 = CInt(Math.Floor((5 - 0 + 1) * Rnd())) + 0   'assigns a random integer between and including 0 and 5
             vaderMove2 = CInt(Math.Floor((5 - 0 + 1) * Rnd())) + 0   'assigns a random integer between and including 0 and 5
-            
+
             If board(vaderMove1, vaderMove2) = 0 Then
                 board(vaderMove1, VaderMove2) = vaderTurn
                 flagFound = True
             End If
-            
+
         Loop While flagFound = False
-        
+
     End Sub
-    
+
     Public Sub medVader()
 
         Dim vaderTurn As Integer                        'variable to hold the numerical value for the AI's turn
@@ -76,10 +75,16 @@
             playerTurn = 1
         End If
 
+        For i As Integer = 0 To 5
+            For j As Integer = 0 To 5
+                If i = 0 Then
 
+                End If
+            Next
+        Next
 
     End Sub
-    
+
     Public Sub hardVader()
         'If vaderflag is hard then run hard algorithm
     End Sub

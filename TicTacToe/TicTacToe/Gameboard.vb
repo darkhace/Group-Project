650c5e68aee82ea2905fb5dabfe5ec96baa39991
Pearlandinjun <sean.m.kruse@gmail.com>
2016-04-13 21:46:04 -0500

    updating Vader
    completed another portion of the CASE statement

--- TicTacToe/TicTacToe/Gameboard.vb
+++ TicTacToe/TicTacToe/Gameboard.vb
@@ -216,16 +216,18 @@
             Case 5 To 10
                 'Utilize 2 square offense from turns 5 to 10
 
+
+
             Case 11 To 18
                 'Utilize 3 square offense from turns 11 to 18
                 
                         For i As Integer = 0 To 5                               'For loop to step through the rows
                             If i = 0 Or i = 3 Then                              'Only scan the first, third, and fourth rows
                                 For j As Integer = 0 To 5                       'Step'For loop to step through each column
-                                    If board(i, j) = playerTurn Then
+                                    If board(i, j) = vaderTurn Then
                                     
                                         'Check to the right
-                                        If board(i, j + 1) = playerTurn And board(i, j + 2) = 0 Then            
+                                        If board(i, j + 1) = vaderTurn And board(i, j + 2) = 0 Then            
                                         
                                             board(i, j + 2) = vaderTurn
                                             loopFlag = True
@@ -233,7 +235,7 @@
                                         End If
                                         
                                         'Check to the bottom right
-                                        If board(i + 1, j + 1) = playerTurn And board(i + 2, j + 2) = 0 Then    
+                                        If board(i + 1, j + 1) = vaderTurn And board(i + 2, j + 2) = 0 Then    
                                         
                                             board(i + 2, j + 2) = vaderTurn
                                             loopFlag = True
@@ -241,7 +243,7 @@
                                         End If
                                         
                                         'Check to the bottom left
-                                        If board(i + 1, j - 1) = playerTurn And board(i + 2, j - 2) = 0 Then    
+                                        If board(i + 1, j - 1) = vaderTurn And board(i + 2, j - 2) = 0 Then    
                                         
                                             board(i + 2, j - 2) = vaderTurn
                                             loopFlag = True
@@ -249,7 +251,7 @@
                                         End If
                                         
                                         'Check below
-                                        If board(i + 1, j) = playerTurn And board(i + 2, j) = 0 Then            
+                                        If board(i + 1, j) = vaderTurn And board(i + 2, j) = 0 Then            
                                         
                                             board(i + 2, j) = vaderTurn
                                             loopFlag = True
@@ -274,8 +276,8 @@
                             For x As Integer = 0 To 5
                                 If x = 1 Or x = 2 Or x = 4 Or x = 5 Then
                                     For y As Integer = 0 To 5
-                                        If board(x, y) = playerTurn Then
-                                            If board(x, y + 1) = playerTurn And board(x, y + 2) = 0 Then
+                                        If board(x, y) = vaderTurn Then
+                                            If board(x, y + 1) = vaderTurn And board(x, y + 2) = 0 Then
                                                 board(x, y + 2) = vaderTurn
                                                 loopFlag = True
                                                 Exit For
@@ -312,6 +314,95 @@
 
             Case 18 To 36
                 'Utilize defense from turn 18 and on
+
+                For i As Integer = 0 To 5                                                      'For loop to step through the rows
+                    If i = 0 Or i = 3 Then                                                     'Only scan the first, third, and fourth rows
+                        For j As Integer = 0 To 5                                              'Step'For loop to step through each column
+                            If board(i, j) = playerTurn Then
+
+                                If board(i, j + 1) = playerTurn And board(i, j + 2) = 0 Then
+                                    'Check to the right
+
+                                    board(i, j + 2) = vaderTurn
+                                    loopFlag = True
+                                    Exit For
+                                End If
+
+                                If board(i + 1, j + 1) = playerTurn And board(i + 2, j + 2) = 0 Then
+                                    'Check to the bottom right
+
+                                    board(i + 2, j + 2) = vaderTurn
+                                    loopFlag = True
+                                    Exit For
+                                End If
+
+                                If board(i + 1, j - 1) = playerTurn And board(i + 2, j - 2) = 0 Then
+                                    'Check to the bottom left
+
+                                    board(i + 2, j - 2) = vaderTurn
+                                    loopFlag = True
+                                    Exit For
+                                End If
+
+                                If board(i + 1, j) = playerTurn And board(i + 2, j) = 0 Then
+                                    'Check below
+
+                                    board(i + 2, j) = vaderTurn
+                                    loopFlag = True
+                                    Exit For
+
+                                End If
+                            End If
+                        Next
+                    End If
+
+                    If loopFlag Then
+                        'To exit the second loop
+
+                        Exit For
+                    End If
+
+                Next
+
+                If loopFlag = False Then
+                    'Loop to test for horizontle scoring on rows 1, 2, 4, and 5
+                    'Only runs if a suitable position isnt found above
+
+                    For x As Integer = 0 To 5
+                        If x = 1 Or x = 2 Or x = 4 Or x = 5 Then
+                            For y As Integer = 0 To 5
+                                If board(x, y) = playerTurn Then
+                                    If board(x, y + 1) = playerTurn And board(x, y + 2) = 0 Then
+                                        board(x, y + 2) = vaderTurn
+                                        loopFlag = True
+                                        Exit For
+                                    End If
+                                End If
+                            Next
+                        End If
+
+                        If loopFlag Then
+                            Exit For
+                        End If
+
+                    Next
+                End If
+
+                Do While flagFound = False
+                    'Do While loop to select a random position and check it for vader's selection. If position is taken the loop iterates again and checks a new position
+
+                    vaderMove1 = CInt(Math.Floor((5 - 0 + 1) * Rnd())) + 0
+                    'assigns a random integer between and including 0 and 5
+
+                    vaderMove2 = CInt(Math.Floor((5 - 0 + 1) * Rnd())) + 0
+                    'assigns a random integer between and including 0 and 5
+
+                    If board(vaderMove1, vaderMove2) = 0 Then
+                        board(vaderMove1, vaderMove2) = vaderTurn
+                        flagFound = True
+                    End If
+
+                Loop
 
         End Select
 
